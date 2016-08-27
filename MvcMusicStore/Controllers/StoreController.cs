9609ad72406b596646345eea25d08f91af0a26bf
Achilles <Achillesy@gmail.com>
2016-08-27 12:30:07 +0800

    第一次提交

---
+++ MvcMusicStore/Controllers/StoreController.cs
@@ -0,0 +1,31 @@
+﻿using MvcMusicStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcMusicStore.Controllers
+{
+    public class StoreController : Controller
+    {
+        MusicStoreEntities storeDB = new MusicStoreEntities();
+
+        //
+        // GET: /Store/
+
+        public ActionResult Index()
+        {
+            var genres = storeDB.Genres.ToList();
+
+            return View(genres);
+        }
+
+        public ActionResult Details(int id)
+        {
+            var album = new Album { Title = "Album " + id };
+
+            return View(album);
+        }
+    }
+}
